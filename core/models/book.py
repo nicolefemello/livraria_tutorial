ce9a4447633cd74b7b9ad66f41703fa212140223
nicolefemello <nicfmello@gmail.com>
2026-02-26 11:05:35 -0300

    feat: add book relationship with category and publisher

--- core/models/book.py
+++ core/models/book.py
@@ -1,10 +1,15 @@
 from django.db import models
 
+from .category import Category
+from .publisher import Publisher
+
 class Book(models.Model):
     title = models.CharField(max_length=255)
     isbn = models.CharField(max_length=32, null=True, blank=True)
     quantity = models.IntegerField(default=0, null=True, blank=True)
     price = models.DecimalField(max_digits=7, decimal_places=2, default=0)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, related_name='books', null=True, blank=True)
+    publisher = models.ForeignKey(Publisher, on_delete=models.PROTECT, related_name='books', null=True, blank=True)
 
     def __str__(self):
         return f'({self.id}) {self.title} ({self.quantity})'
