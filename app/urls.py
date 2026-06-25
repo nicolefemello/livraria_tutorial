de24c1e540799872c4e321b9470e20cd5df4ff25
nicolefemello <nicfmello@gmail.com>
2026-06-25 11:29:02 -0300

    feat: inclusão da app de upload e associação de imagens

--- app/urls.py
+++ app/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import (
@@ -17,6 +19,7 @@ from core.views import (
     AutorViewSet,
     LivroViewSet,
 )
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
 
@@ -48,4 +51,7 @@ urlpatterns = [
     path('api/registro/', UserRegistrationView.as_view(), name='user_registration'),
     # API
     path('api/', include(router.urls)),
+    path('api/media/', include(uploader_router.urls)),
 ]
+
+urlpatterns += static(settings.MEDIA_ENDPOINT, document_root=settings.MEDIA_ROOT)
--- core/serializers/livro.py
+++ core/serializers/livro.py
@@ -1,8 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
 from core.models import Livro
 
 
 class LivroSerializer(ModelSerializer):
+    capa_attachment_key = SlugRelatedField(
+        source='capa',
+        queryset=Image.objects.all(),
+        slug_field='attachment_key',
+        required=False,
+        write_only=True,
+    )
+    capa = ImageSerializer(required=False, read_only=True)
+
     class Meta:
         model = Livro
         fields = '__all__'
@@ -15,6 +27,8 @@ class LivroListSerializer(ModelSerializer):
 
 
 class LivroRetrieveSerializer(ModelSerializer):
+    capa = ImageSerializer(required=False)
+
     class Meta:
         model = Livro
         fields = '__all__'
