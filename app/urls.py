94cd47db8ef1d04891cfd2f9b99a086f5c018f8e
Nicole <166025625+nicolefemello@users.noreply.github.com>
2026-06-25 10:51:56 -0300

    Initial commit

---
+++ app/urls.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+from rest_framework.routers import DefaultRouter
+from core.views import (
+    CustomTokenObtainPairView,
+    CustomTokenRefreshView,
+    CustomTokenVerifyView,
+    UserRegistrationView,
+    UserViewSet,
+)
+
+router = DefaultRouter()
+
+router.register(r'usuarios', UserViewSet, basename='usuarios')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # OpenAPI 3
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path(
+        'api/doc/',
+        SpectacularSwaggerView.as_view(url_name='schema'),
+        name='swagger-ui',
+    ),
+    path(
+        'api/redoc/',
+        SpectacularRedocView.as_view(url_name='schema'),
+        name='redoc',
+    ),
+    # Autenticação JWT
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', CustomTokenVerifyView.as_view(), name='token_verify'),
+    # Registro de usuários
+    path('api/registro/', UserRegistrationView.as_view(), name='user_registration'),
+    # API
+    path('api/', include(router.urls)),
+]
